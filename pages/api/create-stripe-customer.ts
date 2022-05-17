c2e19e99000c518fc41cc298e3c6e5fa08ad68fe
Joonmo You <joonmo.you@gmail.com>
2022-05-17 22:28:23 +0900

    Generate a Custom API Key to Secure an API Route in Next.js

--- pages/api/create-stripe-customer.ts
+++ pages/api/create-stripe-customer.ts
@@ -7,17 +7,25 @@ type Data = {
 }
 
 const secretKey = process.env.STRIPE_SECRET_KEY as string
+const apiSecret = process.env.API_ROUTE_SECRET as string
 
 async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.query.API_ROUTE_SECRET !== apiSecret) {
+    res.status(401).send({ message: 'Unauthorized Request' })
+    return
+  }
+
   const stripe = new Stripe(secretKey, { apiVersion: '2020-08-27' })
   const customer = await stripe.customers.create({
     email: req.body.record.email,
   })
+
   await supabase
     .from('profile')
     .update({ stripe_customer: customer.id })
     .eq('id', req.body.record.id)
-  res.status(200).json({ message: `stripe customer created: ${customer.id}` })
+
+  res.status(200).send({ message: `stripe customer created: ${customer.id}` })
 }
 
 export default handler
