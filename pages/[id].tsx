3b99b329689300560ea172d336d09d7f442a6200
Joonmo You <joonmo.you@gmail.com>
2022-05-19 19:32:03 +0900

    Implement Gated Content Using Row Level Security with Supabase

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,4 +1,6 @@
+import React from 'react'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import ReactPlayer from 'react-player/youtube'
 import { supabase } from '../utils/supabase'
 
 type Lesson = {
@@ -13,10 +15,25 @@ interface Props {
 }
 
 export default function LessonDetails({ lesson }: Props) {
+  const [videoUrl, setVideoUrl] = React.useState<string | null>(null)
+  const getVideoUrl = React.useCallback(async () => {
+    const { data: premium_content } = await supabase
+      .from('premium_content')
+      .select('video_url')
+      .eq('id', lesson.id)
+      .single()
+    setVideoUrl(premium_content?.video_url)
+  }, [])
+
+  React.useEffect(() => {
+    getVideoUrl()
+  }, [])
+
   return (
     <div className="mx-auto w-full max-w-3xl px-8 py-16">
       <h1 className="mb-6 text-3xl">{lesson.title}</h1>
       <p>{lesson.description}</p>
+      {videoUrl ? <ReactPlayer url={videoUrl} width="100%" /> : null}
     </div>
   )
 }
