d99b047f8f1f94a619e8772b020025f14300b92e
Joonmo You <joonmo.you@gmail.com>
2022-05-27 17:31:04 +0900

    Use the Supabase Service Key to Bypass Row Level Security

--- pages/api/stripe-hooks.ts
+++ pages/api/stripe-hooks.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next/types'
 import Stripe from 'stripe'
 import { buffer } from 'micro'
+import { getServiceSupabase } from '../../utils/supabase'
 
 export const config = { api: { bodyParser: false } }
 
@@ -15,15 +16,29 @@ async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const stripe = new Stripe(secretKey, { apiVersion: '2020-08-27' })
   const signature = req.headers['stripe-signature'] as string
   const payload = await buffer(req)
+  let event
 
   try {
-    const event = stripe.webhooks.constructEvent(payload, signature, signingKey)
-    res.status(200).send({ message: `${event}` })
+    event = stripe.webhooks.constructEvent(payload, signature, signingKey)
+    const customer = event.data.object.customer
+    const supabase = getServiceSupabase()
+
+    switch (event.type) {
+      case 'customer.subscription.created':
+        const interval = event.data.object.items.data[0].plan.interval
+        await supabase
+          .from('profile')
+          .update({ is_subscribed: true, interval })
+          .eq('stripe_customer', customer)
+    }
   } catch (error: unknown) {
     if (error instanceof Stripe.errors.StripeSignatureVerificationError) {
       res.status(400).send({ message: `Error ${error?.message}` })
+      return
     }
   }
+
+  res.status(200).send({ message: `${event?.type}` })
 }
 
 export default handler
--- utils/supabase.ts
+++ utils/supabase.ts
@@ -2,5 +2,7 @@ import { createClient } from '@supabase/supabase-js'
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const key = process.env.NEXT_PUBLIC_SUPABASE_KEY as string
+const serviceKey = process.env.SUPABASE_SERVICE_KEY as string
 
 export const supabase = createClient(url, key)
+export const getServiceSupabase = () => createClient(url, serviceKey)
